a3e348366994e0b433c14ffa420b020e64f1a206
AwesomeParley <parleymurdock@gmail.com>
2023-05-13 22:52:00 -0400

    Added text only mode for people with slow hosts

--- BlueyBot.py
+++ BlueyBot.py
@@ -3,6 +3,7 @@ import discord
 import random
 import os
 import configparser
+import time #will be used for daily in future update.
 from sys import exit
 
 # Check if the settings file exists, if not create it
@@ -13,8 +14,9 @@ def create_default_settings():
         'Seconds to respond': '120',
         'Daily': 'False',
         'Command': '!guess',
-        'Image spoilers': 'False',
-        'Reactions': 'True'
+        'Spoilers': 'False',
+        'Reactions': 'True',
+        'Text only': 'False'
     }
     with open('settings.txt', 'w') as configfile:
         config.write(configfile)
@@ -27,8 +29,9 @@ def read_settings():
         'Seconds to respond': config.getint('DEFAULT', 'Seconds to respond'),
         'Daily': config.getboolean('DEFAULT', 'Daily'),
         'Command': config.get('DEFAULT', 'Command'),
-        'Image spoilers': config.getboolean('DEFAULT', 'Image spoilers'),
+        'Spoilers': config.getboolean('DEFAULT', 'Spoilers'),
         'Reactions': config.getboolean('DEFAULT', 'Reactions'),
+        'Text only': config.getboolean('DEFAULT', 'Text only'),
     }
     return settings
 
@@ -40,18 +43,24 @@ token = settings['Token']
 seconds_to_respond = settings['Seconds to respond']
 daily = settings['Daily'] #To Be Implemented
 command = settings['Command']
-spoilers = settings['Image spoilers']
+spoilers = settings['Spoilers']
 reactions = settings['Reactions']
+textOnly = settings['Text only']
 
 if(token == 'Insert Bot Token Here'):
     print("change \"Insert Bot Token Here\" in setting.txt to your discord token before running!")
+    time.sleep(30) # added this so people using the exe or just running the python on it's own can read the message above.
     exit()
 else:
     intents = discord.Intents.all()
     client = discord.Client(intents=intents)
 
-episodes_file = "episodes.txt"
-max_attempts = 5 #Only change if you want to use your own images, and you have a different number of them.
+if not textOnly:
+    episodes_file = "episodes.txt"
+    max_attempts = 1 #Only change if you want to use your own images, and you have a different number of them.
+else:
+    episodes_file = "episodes_in_order.txt"
+    max_attempts = 2 #Only change if you want to use your own text, and you have a different number of them.
 
 @client.event
 async def on_ready():
@@ -62,19 +71,40 @@ async def on_message(message):
     if message.author == client.user:
         return
     if message.content == command:
-        x = random.randint(1, 87)
-        while x == 44 or x == 45: #Will fix eventually, but for right now, just don't use those
-            x = random.randint(1, 87)
+        if not textOnly:
+            x = random.randint(1, 87) #There are 87 episodes with images supplied by Blueydle currently.
+            while x == 44 or x == 45: #Will fix eventually, but for right now, just don't use those
+                x = random.randint(1, 87)
+        else:
+            x = random.randint(1, 147) #There are 147 episodes avalible currently.
         y = 1
         while y <= max_attempts:
             episode = get_episode(x)
-            filename = f'images\\{x}_{y}.jpg' # Format the filename string with x and y
-            with open(filename, 'rb') as f:
-                if spoilers: 
-                    file = discord.File(f, filename='SPOILER_nice_try.jpg') # Named "SPOILER_nice_try.jpg" so that the smart people get trolled, and the image is set as a spoiler.
+            if not textOnly:
+                filename = f'images\\{x}_{y}.jpg' # Format the filename string with x and y
+                with open(filename, 'rb') as f:
+                    if spoilers: 
+                        file = discord.File(f, filename='SPOILER_nice_try.jpg') # Named "SPOILER_nice_try.jpg" so that the smart people get trolled, and the image is set as a spoiler.
+                    else:
+                        file = discord.File(f, filename='nice_try.jpg') # Named "nice_try.jpg" so that the smart people get trolled.
+                if not max_attempts == 1:
+                    guess_amount = f'You are on guess {y}/{max_attempts}. '
                 else:
-                    file = discord.File(f, filename='nice_try.jpg') # Named "nice_try.jpg" so that the smart people get trolled.
-                await message.reply(f'You are on guess {y}. Guess the episode.', file=file)
+                    guess_amount = ''
+                await message.reply(f'{guess_amount}Guess the episode.', file=file)
+            else:
+                with open('episode_descriptions.txt') as f:
+                    episode_descriptions = f.readlines()
+                    episode_description = episode_descriptions[x-1] if len(episode_descriptions) >= x else None
+                    description = episode_description.strip() if episode_description else None
+                if not max_attempts == 1:
+                    guess_amount = f'You are on guess {y}/{max_attempts}. '
+                else:
+                    guess_amount = ''
+                if spoilers:
+                    await message.reply(f'{guess_amount}Guess the episode:\n\n||{description}||')
+                else:
+                    await message.reply(f'{guess_amount}Guess the episode:\n\n{description}')
             
             def check(m):
                 return m.author == message.author and m.channel == message.channel
